02098e5ae3572bc71309df0323b0927184d4876c
Archie9010 <archie9010@protonmail.com>
2022-10-18 14:36:49 +0000

    add code examples

--- destructuring.js
+++ destructuring.js
@@ -3,10 +3,42 @@
  * command node destructuring.js in the terminal
  */
 
-// Destructuring arrays
+// Destructuring arrays 
+let ages = [30, 26, 27];
+// let john = ages[0];
+// let marry = ages[1];
+// let joe = ages[2]; 
+let [john, marry, joe] = ages;
+console.log[john, marry, joe];
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
 
 // Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+let [,,marryNative, marrySecondary] = languages;
+console.log(marryNative, marrySecondary);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "spanish",
+    fourthLanguage: "german",
+    fifthLanguage: "japanese",
+};
+let { firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
 
 // Using rest parameter syntax
+let fruits = ["apple", "bannana", "peach", "cherry"];
+let [favorite, secondFavorite, ...others] = fruits;
+console.lo
